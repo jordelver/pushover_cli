55a999fd01641d1081be50657429c523ba0da777
Jordan Elver <jordan@elver.me>
2022-02-04 19:42:26 +0000

    Setup integration tests to cover cli flags
    More coming, but these are simple and can be tested without having to
    figure out any mocking of network requests etc.

---
+++ tests/integration_test.rs
@@ -0,0 +1,37 @@
+use assert_cmd::Command;
+
+#[test]
+fn test_help_flag() {
+    let mut cmd = Command::cargo_bin("pushover_cli").unwrap();
+    let output = r#"pushover_cli 0.1.0
+Send notifications using Pushover
+
+USAGE:
+    pushover_cli --user <USER> --token <TOKEN> --message <MESSAGE>
+
+OPTIONS:
+    -h, --help                 Print help information
+    -m, --message <MESSAGE>    Message to send
+    -t, --token <TOKEN>        Pushover token
+    -u, --user <USER>          Pushover user
+    -V, --version              Print version information
+"#;
+
+    let assert = cmd.arg("--help").assert();
+    assert.success().code(0).stdout(output);
+
+    let assert = cmd.arg("-h").assert();
+    assert.success().code(0).stdout(output);
+}
+
+#[test]
+fn test_version_flag() {
+    let mut cmd = Command::cargo_bin("pushover_cli").unwrap();
+    let output = "pushover_cli 0.1.0\n";
+
+    let assert = cmd.arg("--version").assert();
+    assert.success().code(0).stdout(output);
+
+    let assert = cmd.arg("-V").assert();
+    assert.success().code(0).stdout(output);
+}
