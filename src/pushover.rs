f570771397a4f975ec406099f861b828766fc259
Jordan Elver <jordan@elver.me>
2022-02-06 22:06:08 +0000

    Remove some single use `use` statements
    Not sure what is idiomatic here.

--- src/pushover.rs
+++ src/pushover.rs
@@ -5,16 +5,13 @@ mod response_body;
 use reqwest::blocking::Client;
 use reqwest::StatusCode;
 
-use crate::cli::Args;
 use error::PushoverError;
-use error::PushoverError::ApiError;
-use payload::Payload;
 use response_body::ResponseBody;
 
 static PUSHOVER_API_ENDPOINT: &str = "https://api.pushover.net/1/messages.json";
 
-pub fn send_notification(args: Args) -> Result<(), PushoverError> {
-    let payload = Payload::new(args);
+pub fn send_notification(args: super::cli::Args) -> Result<(), PushoverError> {
+    let payload = payload::Payload::new(args);
 
     let response = Client::new()
         .post(PUSHOVER_API_ENDPOINT)
@@ -26,6 +23,6 @@ pub fn send_notification(args: Args) -> Result<(), PushoverError> {
 
     match status {
         StatusCode::OK => Ok(()),
-        _ => Err(ApiError(body)),
+        _ => Err(PushoverError::ApiError(body)),
     }
 }
